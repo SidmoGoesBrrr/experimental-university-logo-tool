de019f32aba2f5f2c1a89c37477e6ba87a7042c3
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-11-07 14:31:36 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-from googlesearch import search
 import os
 import zipfile
 import logging
+from bing_image_downloader import downloader
 
 # Configure the logging settings
 logging.basicConfig(
@@ -16,22 +14,9 @@ logging.basicConfig(
 # Function to search for university logo
 def search_university_logo(university):
     try:
-        query = f"{university} logo site:.edu"  # Search for university logo on .edu domains
-        for j in search(query, num_results=1):
-            page = requests.get(j)
-            soup = BeautifulSoup(page.content, 'html.parser')
-            logo = soup.find("img", {"alt": f"{university} logo"})
-            if logo:
-                return logo['src']
-
-        # If not found on . .edu domain, try searching on Google Images
-        query = f"{university} logo"
-        for j in search(query, num=1, stop=1, pause=2):
-            return j
-
+        downloader.download(f"{university} logo", limit=1, output_dir='uni_logos', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
     except Exception as e:
         logging.error(f"Error searching for logo for {university}: {str(e)}")
-        return str(e)
 
 # Streamlit app
 st.title("University Logo Scraper")
@@ -48,38 +33,32 @@ if uploaded_file is not None:
             st.write(df)
 
             # Create a temporary directory to store downloaded logos
-            os.makedirs("temp", exist_ok=True)  # Use exist_ok=True to avoid errors if the directory already exists
+            os.makedirs("uni_logos", exist_ok=True)  # Use exist_ok=True to avoid errors if the directory already exists
 
             logo_urls = {}
             for university in df["University"]:
-                logo_url = search_university_logo(university)
-                if logo_url:
-                    logo_urls[university] = logo_url
+                search_university_logo(university)
+                logo_filename = f"{university} logo/logo.jpg"  # Bing Image Downloader stores images in a subdirectory
+                if os.path.exists(logo_filename):
+                    logo_urls[university] = logo_filename
 
             # Zip the logos
             zip_filename = "university_logos.zip"
             with zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
                 exceptions = {}
-                for university, logo_url in logo_urls.items():
+                for university, logo_filename in logo_urls.items():
                     try:
-                        logo = requests.get(logo_url)
-                        if logo.status_code == 200:
-                            with open(os.path.join("temp", f"{university}.png"), "wb") as f:
-                                f.write(logo.content)
-                            zipf.write(os.path.join("temp", f"{university}.png"), f"{university}.png")
-                            logging.info(f"Fetched logo for {university}")
-                        else:
-                            exceptions[university] = f"Failed to fetch logo (Status code {logo.status_code})"
-                            logging.warning(f"Failed to fetch logo for {university}: Status code {logo.status_code}")
+                        zipf.write(logo_filename, f"{university}.jpg")
+                        logging.info(f"Fetched logo for {university}")
                     except Exception as e:
                         exceptions[university] = str(e)
                         logging.error(f"Error fetching logo for {university}: {str(e)}")
 
             # Display logos and exceptions
-            for university, logo_url in logo_urls.items():
+            for university, logo_filename in logo_urls.items():
                 st.write(f"Logo for {university}:")
                 try:
-                    st.image(logo_url, use_column_width=True)
+                    st.image(logo_filename, use_column_width=True)
                 except:
                     pass
 
@@ -100,13 +79,6 @@ if uploaded_file is not None:
                     mime="application/octet-stream"
                 )
 
-            # Cleanup temporary directory
-            for file_name in os.listdir("temp"):
-                file_path = os.path.join("temp", file_name)
-                if os.path.isfile(file_path):
-                    os.remove(file_path)
-            os.rmdir("temp")
-
         else:
             st.write("CSV file must contain a column named 'University'.")
 
@@ -115,4 +87,3 @@ if uploaded_file is not None:
 
 # Log when the app finishes
 logging.info("University Logo Scraper finished")
-
